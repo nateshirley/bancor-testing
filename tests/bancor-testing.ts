3c81a44edd04395611a2f2ba4587dba9e1f4d132
Nathan Shirley <nshirley22@gmail.com>
2022-01-22 16:49:06 -0500

    second day

--- tests/bancor-testing.ts
+++ tests/bancor-testing.ts
@@ -2,6 +2,9 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { BancorTesting } from "../target/types/bancor_testing";
 
+export const TARGET_DECIMALS = 6;
+export const TARGET_DECIMAL_MODIFIER = 10 ** TARGET_DECIMALS;
+
 describe("bancor-testing", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
@@ -20,8 +23,8 @@ describe("bancor-testing", () => {
     reserveRatio: number;
   }
   let market: Market = {
-    targetTokenSupply: 10000,
-    reserveBalance: 1000,
+    targetTokenSupply: 100000 * TARGET_DECIMAL_MODIFIER,
+    reserveBalance: 1 * TARGET_DECIMAL_MODIFIER,
     reserveRatio: 0.5,
   };
   interface User {
@@ -29,14 +32,19 @@ describe("bancor-testing", () => {
     targetTokenBalance: number;
   }
   let user: User = {
-    reserveTokenBalance: 100000,
+    reserveTokenBalance: 20000 * TARGET_DECIMAL_MODIFIER,
     targetTokenBalance: 0,
   };
 
   it("testing", async () => {
     /*
-    treasury / (supply * crr) = price
+
+    reserve ratio maintains a constant between the token's market cap and its treasury reserves
+    if you assume that the funds going into the treasury will match 1:1 with the tokens coming out of the reserves
+    then this can be modeled like a normal bonding curve
+
     //tokens received is tokens received by the curve
+
 
     //
     PurchaseReturn = ContinuousTokenSupply * ((1 + ReserveTokensReceived / ReserveTokenBalance) ^ (ReserveRatio) - 1)
@@ -47,16 +55,63 @@ describe("bancor-testing", () => {
     */
     printMarketStatus(market);
 
-    buyTarget(5000, market, user);
-    buyTarget(5000, market, user);
-    fillReserve(market, 250);
-    //printMarketStatus(market);
-    buyTarget(200, market, user);
-    fillReserve(market, 350);
-    sellTarget(300.15315780902193, market, user);
-    sellTarget(120, market, user);
-    sellTarget(120, market, user);
+    // buy(1000 * TARGET_DECIMAL_MODIFIER, market, user);
+    // buy(3000 * TARGET_DECIMAL_MODIFIER, market, user);
+    // buy(30 * TARGET_DECIMAL_MODIFIER, market, user);
+    // sell(30 * TARGET_DECIMAL_MODIFIER, market, user);
+    // sell(3000 * TARGET_DECIMAL_MODIFIER, market, user);
+    buy(100000 * TARGET_DECIMAL_MODIFIER, market, user);
+    buy(100000 * TARGET_DECIMAL_MODIFIER, market, user);
+    buy(100000 * TARGET_DECIMAL_MODIFIER, market, user);
+    buy(100000 * TARGET_DECIMAL_MODIFIER, market, user);
+    buy(100000 * TARGET_DECIMAL_MODIFIER, market, user);
+    buy(100000 * TARGET_DECIMAL_MODIFIER, market, user);
+
+    //sell(100000, market, user);
+
+    // fillReserve(market, 250);
+    // //printMarketStatus(market);
+    // buyTarget(200, market, user);
+    // fillReserve(market, 350);
+    // //what im trying to see i guess is how the reserve fill will affect cashing out
+    // sellTarget(300.15315780902193, market, user);
+    // sellTarget(120, market, user);
+    // sellTarget(120, market, user);
+
+    /*
+    so what else do i need
+    - supply caps
+    so instead of sponsored burning, u would just add to the treasury and you're good to go
+    and then if someone redeems, then someone else can step in and mint
+
+    */
   });
+
+  const buy = (targets: number, market: Market, user: User) => {
+    console.log("");
+    console.log("BUYING");
+    console.log("targets received:", targets);
+    let reserveChange = reserveValueOnBuy(targets, market);
+    market.reserveBalance += reserveChange;
+    market.targetTokenSupply += targets;
+    user.reserveTokenBalance -= reserveChange;
+    user.targetTokenBalance += targets;
+    console.log("price per token: ", reserveChange / targets, "reserve");
+    printMarketStatus(market);
+  };
+
+  const sell = (targets: number, market: Market, user: User) => {
+    console.log("");
+    console.log("SELLING");
+    console.log("targets sold:", targets);
+    let reserveChange = reserveValueOnSell(targets, market);
+    market.reserveBalance -= reserveChange;
+    market.targetTokenSupply -= targets;
+    user.reserveTokenBalance += reserveChange;
+    user.targetTokenBalance -= targets;
+    console.log("price per token: ", reserveChange / targets, "reserve");
+    printMarketStatus(market);
+  };
 
   const fillReserve = (market: Market, amount: number) => {
     console.log("");
@@ -66,41 +121,6 @@ describe("bancor-testing", () => {
     let newMarginalPrice = marginalPrice(market);
     console.log("marginal price change: ", newMarginalPrice - oldMarginalPrice);
   };
-
-  const buyTarget = (reserveToSpend: number, market: Market, user: User) => {
-    console.log("");
-    console.log("BUYING");
-    console.log("reserve given:", reserveToSpend);
-    let targetsReceived = getTargetsReceieved(reserveToSpend, market);
-    market.reserveBalance += reserveToSpend;
-    market.targetTokenSupply += targetsReceived;
-    user.reserveTokenBalance -= reserveToSpend;
-    user.targetTokenBalance += targetsReceived;
-    console.log(
-      "price per token: ",
-      reserveToSpend / targetsReceived,
-      "reserve"
-    );
-    printMarketStatus(market);
-  };
-
-  const sellTarget = (targetsToGive: number, market: Market, user: User) => {
-    console.log("");
-    console.log("SELLING");
-    console.log("targets given:", targetsToGive);
-    let reserveReceived = getReserveReceived(targetsToGive, market);
-    market.reserveBalance -= reserveReceived;
-    market.targetTokenSupply -= targetsToGive;
-    user.reserveTokenBalance += reserveReceived;
-    user.targetTokenBalance -= targetsToGive;
-    console.log(
-      "price per token: ",
-      reserveReceived / targetsToGive,
-      "reserve"
-    );
-    printMarketStatus(market);
-  };
-
   const printMarketStatus = (market: Market) => {
     console.log("MARKET STATUS ------");
     let status = {
@@ -111,11 +131,54 @@ describe("bancor-testing", () => {
     };
     console.log(status);
   };
+
+  //treasury / (supply * reserveRatio) = price
   const marginalPrice = (market: Market) => {
     return (
-      market.reserveBalance / (market.targetTokenSupply * market.reserveRatio)
+      (market.reserveBalance + targetModifier) /
+      (market.targetTokenSupply * market.reserveRatio)
     );
   };
+
+  //reserveValue = collateral * ((1 + targets / targetSupply)^(1/reserveRatio) — 1)
+  const reserveValueOnBuy = (targets: number, market: Market) => {
+    let base = 1 + targets / market.targetTokenSupply;
+    let ex = Math.pow(base, 1 / market.reserveRatio) - 1;
+    let whole = (market.reserveBalance + targetModifier) * ex;
+    console.log("reserve value on buy: ", whole);
+    return whole;
+  };
+  const targetModifier = 1000000000;
+  //reserveValue = collateral * (1 - (1 - targets / targetSupply)^ (1/reserveRatio)))
+  const reserveValueOnSell = (targets: number, market: Market) => {
+    let base = 1 - targets / market.targetTokenSupply;
+    let ex = 1 - Math.pow(base, 1 / market.reserveRatio);
+    let whole = (market.reserveBalance + targetModifier) * ex;
+    console.log("reserve value on sell: ", whole);
+    return whole;
+  };
+
+  /*
+
+)
+  
+  "receive x target tokens and pay y reserve" -- this is same as sell amount from billy (billy is from curve POV)
+  reserveValueOnBuy = collateral * ((1 + tokensSold / totalSupply)^(1/ReserveRatio) — 1)
+  
+
+  "give back x tokens and receive y reserves" -- this is same as saleReturn from yos
+  reserveValueOnSell = collateral * (1 - (1 - tokensSold / tokenSupply) ^ (1 / ReserveRatio))
+
+
+  i think the problem is billy is doing it from the perspective of the curve only
+  and the other guy is flip flopping
+
+  */
+
+  //buyAmt = tokenSupply * ((1 + amtPaid / collateral)^CW — 1)
+  //SaleReturn = ReserveTokenBalance * (1 - (1 - ContinuousTokensReceived / ContinuousTokenSupply) ^ (1 / (ReserveRatio)))
+  //derived from each
+  //PurchaseReturn = ContinuousTokenSupply * ((1 + ReserveTokensReceived / ReserveTokenBalance) ^ (ReserveRatio) - 1)
 
   //    PurchaseReturn = ContinuousTokenSupply * ((1 + ReserveTokensReceived / ReserveTokenBalance) ^ (ReserveRatio) - 1)
   // buyAmt = tokenSupply * ((1 + amtPaid / collateral)^CW — 1)
@@ -134,7 +197,116 @@ describe("bancor-testing", () => {
     console.log("targets received: ", whole);
     return whole;
   };
+});
 
+/*
+
+  bancor-testing
+MARKET STATUS ------
+{
+  targetTokenSupply: 1000000,
+  reserveBalance: 100000,
+  reserveRatio: 0.5,
+  marginalPrice: 0.2
+}
+
+BUYING
+targets received: 100000
+reserve value on buy:  21000.00000000002
+price per token:  0.2100000000000002 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 1100000,
+  reserveBalance: 121000.00000000001,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22000000000000003
+}
+
+BUYING
+targets received: 40000
+reserve value on buy:  8960.00000000001
+price per token:  0.22400000000000023 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 1140000,
+  reserveBalance: 129960.00000000003,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22800000000000006
+}
+
+BUYING
+targets received: 80000
+reserve value on buy:  18879.999999999996
+price per token:  0.23599999999999996 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 1220000,
+  reserveBalance: 148840.00000000003,
+  reserveRatio: 0.5,
+  marginalPrice: 0.24400000000000005
+}
+
+SELLING
+targets sold: 80000
+reserve value on sell:  18880
+price per token:  0.236 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 1140000,
+  reserveBalance: 129960.00000000003,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22800000000000006
+}
+
+SELLING
+targets sold: 40000
+reserve value on sell:  8960.000000000004
+price per token:  0.2240000000000001 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 1100000,
+  reserveBalance: 121000.00000000003,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22000000000000006
+}
+
+SELLING
+targets sold: 100000
+reserve value on sell:  21000.000000000015
+price per token:  0.21000000000000016 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 1000000,
+  reserveBalance: 100000.00000000001,
+  reserveRatio: 0.5,
+  marginalPrice: 0.20000000000000004
+}
+*/
+
+/*
+
+-- derived from buy amt? switching places
+  "pay y reserve and receive x target tokens"
+  tokenValue = tokenSupply * ((1 + reservesPaid / collateral)^CW — 1
+
+"receive x reserves and sell y tokens"
+  reserveValue = collateral * ((1 + tokensSold / totalSupply)^(1/CW) — 1)
+  const buyTarget = (reserveToSpend: number, market: Market, user: User) => {
+    console.log("");
+    console.log("BUYING");
+    console.log("reserve given:", reserveToSpend);
+    let targetsReceived = getTargetsReceieved(reserveToSpend, market);
+    market.reserveBalance += reserveToSpend;
+    market.targetTokenSupply += targetsReceived;
+    user.reserveTokenBalance -= reserveToSpend;
+    user.targetTokenBalance += targetsReceived;
+    console.log(
+      "price per token: ",
+      reserveToSpend / targetsReceived,
+      "reserve"
+    );
+    printMarketStatus(market);
+  };
   //sellAmt = collateral * ((1 + tokensSold / totalSupply)^(1/CW) — 1)
   const getReserveReceived = (targetTokensSold: number, market: Market) => {
     let base = 1 - targetTokensSold / market.targetTokenSupply;
@@ -143,4 +315,89 @@ describe("bancor-testing", () => {
     console.log("reserve received: ", whole);
     return whole;
   };
-});
+  const sellTarget = (targetsToGive: number, market: Market, user: User) => {
+    console.log("");
+    console.log("SELLING");
+    console.log("targets given:", targetsToGive);
+    let reserveReceived = getReserveReceived(targetsToGive, market);
+    market.reserveBalance -= reserveReceived;
+    market.targetTokenSupply -= targetsToGive;
+    user.reserveTokenBalance += reserveReceived;
+    user.targetTokenBalance -= targetsToGive;
+    console.log(
+      "price per token: ",
+      reserveReceived / targetsToGive,
+      "reserve"
+    );
+    printMarketStatus(market);
+  };
+
+
+MARKET STATUS ------
+{
+  targetTokenSupply: 10000,
+  reserveBalance: 1000,
+  reserveRatio: 0.5,
+  marginalPrice: 0.2
+}
+
+BUYING
+targets received: 1000
+reserve value:  210.0000000000002
+price per token:  0.2100000000000002 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 11000,
+  reserveBalance: 1210.0000000000002,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22000000000000003
+}
+
+BUYING
+targets received: 400
+reserve value:  89.60000000000011
+price per token:  0.22400000000000028 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 11400,
+  reserveBalance: 1299.6000000000004,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22800000000000006
+}
+
+BUYING
+targets received: 800
+reserve value:  188.79999999999998
+price per token:  0.236 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 12200,
+  reserveBalance: 1488.4000000000003,
+  reserveRatio: 0.5,
+  marginalPrice: 0.24400000000000005
+}
+
+SELLING
+targets given: 800
+reserve received:  188.8
+price per token:  0.23600000000000002 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 11400,
+  reserveBalance: 1299.6000000000004,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22800000000000006
+}
+
+SELLING
+targets given: 400
+reserve received:  89.60000000000004
+price per token:  0.2240000000000001 reserve
+MARKET STATUS ------
+{
+  targetTokenSupply: 11000,
+  reserveBalance: 1210.0000000000002,
+  reserveRatio: 0.5,
+  marginalPrice: 0.22000000000000003
+}
+*/
